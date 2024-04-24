51f6de9d0ea823d725a4ad92ed40fd5d48cd229e
Alex Deng <alex.deng2002@gmail.com>
2024-04-23 21:13:28 -0400

    fix problems

--- frontend/mental_health_app/src/components/QuestionOne.js
+++ frontend/mental_health_app/src/components/QuestionOne.js
@@ -12,14 +12,14 @@ function QuestionOne() {
       <div className="flex w-full h-full px-20">
         <button
           onClick={() => navigate("/negative")}
-          className="w-1/2 mr-4 text-4xl font-bold transition duration-500 ease-in-out transform border-4 border-red-600 rounded-lg shadow-xl h-3/4 bg-gradient-to-bl from-gray-800 to-black hover:scale-105 hover:bg-gradient-to-br from-red-800 to-red-600 hover:text-white hover:shadow-2xl"
+          className="w-1/2 mr-4 text-4xl font-bold transition duration-500 ease-in-out transform border-4 border-red-600 rounded-lg shadow-xl h-3/4 bg-gradient-to-bl hover:scale-105 hover:bg-gradient-to-br from-red-800 to-red-600 hover:text-white hover:shadow-2xl"
           style={{ backdropFilter: "blur(10px)" }}
         >
           Bad
         </button>
         <button
           onClick={() => navigate("/positive")}
-          className="w-1/2 text-4xl font-bold transition duration-500 ease-in-out transform border-4 border-green-600 rounded-lg shadow-xl h-3/4 bg-gradient-to-bl from-gray-800 to-black hover:scale-105 hover:bg-gradient-to-br from-green-500 to-green-300 hover:text-white hover:shadow-2xl"
+          className="w-1/2 text-4xl font-bold transition duration-500 ease-in-out transform border-4 border-green-600 rounded-lg shadow-xl h-3/4 bg-gradient-to-bl hover:scale-105 hover:bg-gradient-to-br from-green-500 to-green-300 hover:text-white hover:shadow-2xl"
           style={{ backdropFilter: "blur(10px)" }}
         >
           Good
