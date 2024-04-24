eb69b3edb08337fc718d9a15359b42fe9e1e3563
Alex Deng <alex.deng2002@gmail.com>
2024-04-23 20:37:05 -0400

    app changes

--- frontend/mental_health_app/src/components/Navbar.js
+++ frontend/mental_health_app/src/components/Navbar.js
@@ -1,21 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
-import PeopleIcon from "@mui/icons-material/People";
-import ChatIcon from "@mui/icons-material/Chat";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const goForward = () => {
+    navigate(1);
+  };
+
   return (
     <nav className="flex items-center justify-between p-4 text-white bg-black shadow-md bg-opacity-80">
-      <Link to="/" className="transition hover:brightness-125">
+      <button
+        onClick={goBack}
+        className="flex items-center transition hover:brightness-125"
+      >
+        <ArrowBackIcon />
+        Go Back
+      </button>
+      <Link
+        to="/"
+        className="flex items-center transition hover:brightness-125"
+      >
+        <HomeIcon />
         Home
       </Link>
-      <Link to="/people" className="transition hover:brightness-125">
-        People Tracker
-      </Link>
-      <Link to="/websocket" className="transition hover:brightness-125">
-        WebSocket
-      </Link>
+      <button
+        onClick={goForward}
+        className="flex items-center transition hover:brightness-125"
+      >
+        Go Forward
+        <ArrowForwardIcon />
+      </button>
     </nav>
   );
 };
